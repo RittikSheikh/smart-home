61b594cd2b0c34b2f700f83fe999cec56db4ee28
rittik sheikh <rittik11500@gamil.com>
2023-03-02 09:59:40 +0600

    first commit

---
+++ src/components/Router/Router.js
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layouts/Main";
+import Home from "../Home";
+
+
+const router = createBrowserRouter([
+    {path:'/',element:<Main/>,children:[
+        {path:'/',element:<Home/>}
+    ]}
+])
+
+export default router;
